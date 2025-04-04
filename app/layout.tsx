b5d551cfed926e0b62be2ad2425c9d58fa46e132
Tornaid <alexandre.steinmetz5@gmail.com>
2025-04-04 16:42:26 +0200

    add keywords SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,45 @@
 import "./globals.css";
 
 export const metadata = {
-  title: 'ReqVet - Simplifiez vos consultations vétérinaires',
-  description: 'Une application conçue pour libérer les vétérinaires de la rédaction de leurs comptes-rendus, simple, rapide et efficace.',
+  title: 'ReqVet - Assistant IA pour vétérinaires',
+  description: 'ReqVet est une application pour vétérinaires qui automatise la rédaction des comptes-rendus de consultation grâce à l’intelligence artificielle. Gagnez du temps, améliorez la traçabilité.',
+  keywords: [
+    "application vétérinaire",
+    "assistant vétérinaire",
+    "intelligence artificielle vétérinaire",
+    "rédaction compte rendu consultation",
+    "compte-rendu vétérinaire",
+    "IA santé animale",
+    "automatisation vétérinaire",
+    "outil pour vétérinaire",
+    "ReqVet",
+  ],
   icons: {
     icon: '/favicon.ico', 
   },
   openGraph: {
-    title: 'ReqVet',
-    description: 'Générez automatiquement vos comptes-rendus vétérinaires grâce à l’intelligence artificielle.',
+    title: 'ReqVet - Assistant IA pour vétérinaires',
+    description: 'Automatisez vos comptes-rendus de consultation vétérinaire grâce à ReqVet, une application IA simple, rapide et efficace.',
     url: 'https://reqvet.com',
     siteName: 'ReqVet',
     images: [
       {
-        url: '/opengraph-image.jpg', 
+        url: '/opengraph-image.png',
         width: 1200,
         height: 630,
-        alt: 'ReqVet - Simplifiez vos consultations',
+        alt: 'ReqVet - Assistant vétérinaire intelligent',
       },
     ],
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'ReqVet',
-    description: 'Une application conçue pour simplifier les consultations vétérinaires.',
-    images: ['/opengraph-image.jpg'],
+    title: 'ReqVet - Gagnez du temps sur vos consultations vétérinaires',
+    description: 'Automatisez vos comptes-rendus de consultation vétérinaire grâce à ReqVet, une application IA simple, rapide et efficace.',
+    images: ['/opengraph-image.png'],
   },
 };
+
 
 export default function RootLayout({
   children,
