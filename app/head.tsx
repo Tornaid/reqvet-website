32a5faf4e2e8dcccc18d7a1e9984fb6042c6e197
Tornaid <alexandre.steinmetz5@gmail.com>
2025-04-09 14:39:38 +0200

    add app/head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+// app/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>ReqVet - Simplifiez la rédaction de vos comptes-rendus vétérinaires</title>
+        <meta name="description" content="ReqVet est une application pour vétérinaires qui automatise la rédaction des comptes-rendus de consultation grâce à l’intelligence artificielle." />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    );
+  }
+  
